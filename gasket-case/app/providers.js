981a888ddde0792489018e842908c2f82e6f4e75
Daniel Van Bueren <danvb10@gmail.com>
2026-07-15 21:44:20 -0500

    Add vercel analytics

--- gasket-case/app/providers.js
+++ gasket-case/app/providers.js
@@ -5,16 +5,18 @@ import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { SessionProvider } from 'next-auth/react'
 import theme from './theme'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Providers({ children }) {
-  return (
-      <EmotionRegistry>
-          <SessionProvider>
-              <ThemeProvider theme={theme}>
-                  <CssBaseline />
-                  {children}
-              </ThemeProvider>
-          </SessionProvider>
-      </EmotionRegistry>
-  )
+    return (
+        <EmotionRegistry>
+            <SessionProvider>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    {children}
+                    <Analytics />
+                </ThemeProvider>
+            </SessionProvider>
+        </EmotionRegistry>
+    )
 }
